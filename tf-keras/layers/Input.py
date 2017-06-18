50ebfe247ea5d09ff149e849f02b8d7f860eea54
EmbraceLife <1227561934@qq.com>
2017-06-18 23:08:25 +0800

    update

--- tf-keras/layers/Input.py
+++
@@ -1,8 +0,0 @@
-tensorflow.contrib.keras.python.keras.layers
-__init__.py
-
-# borrow Input, InputLayer, InputSpec, Layer from engine.py below
-from tensorflow.contrib.keras.python.keras.engine import Input
-from tensorflow.contrib.keras.python.keras.engine import InputLayer
-from tensorflow.contrib.keras.python.keras.engine import InputSpec
-from tensorflow.contrib.keras.python.keras.engine import Layer
